c93848d1e0404e7f45acdc72e13416ad9c391f9e
kev127 <kelvinkeya125@gmail.com>
2020-10-14 15:31:50 +0300

    updated code

--- app/request.py
+++ app/request.py
@@ -16,7 +16,7 @@ def get_movies(category):
     Function that gets the json response to our url request
     '''
     get_movies_url = base_url.format(category,api_key)
-    import pdb;pdb set trace
+
     with urllib.request.urlopen(get_movies_url) as url:
         get_movies_data = url.read()
         get_movies_response = json.loads(get_movies_data)
@@ -54,3 +54,22 @@ def process_results(movie_list):
             movie_results.append(movie_object)
 
     return movie_results
+
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+return movie_object
