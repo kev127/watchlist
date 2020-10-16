a683cc1e3d8966e615992534ad0905c88df20f27
kev127 <kelvinkeya125@gmail.com>
2020-10-16 10:34:38 +0300

    update code

--- app/views.py
+++ app/views.py
@@ -1,9 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
-from .models import review
-from .forms import ReviewForm
-Review = review.Review
 
 @app.route('/')
 def index():
@@ -33,8 +30,9 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -47,17 +45,3 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-def new_review(id):
-    form = ReviewForm()
-    movie = get_movie(id)
-
-    if form.validate_on_submit():
-        title = form.title.data
-        review = form.review.data
-        new_review = Review(movie.id,title,movie.poster,review)
-        new_review.save_review()
-        return redirect(url_for('movie',id = movie.id ))
-
-    title = f'{movie.title} review'
-    return render_template('new_review.html',title = title, review_form=form, movie=movie)
